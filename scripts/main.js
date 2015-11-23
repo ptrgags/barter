cb3d1958e6396c02aca8a65598ef72f05e698c8f
Peter Gagliardi <ptrgags@live.com>
2015-11-23 04:53:26 -0500

    Add old Barter code
    Added code for Barter that represents the state of the project
    at the end of high school.

---
+++ scripts/main.js
@@ -0,0 +1,176 @@
+﻿//Setup the program
+
+//Barter namespace
+var barter = {};
+
+//Read raw data
+readItems(itemsRaw);
+readOptions(optionsRaw);
+readSituations(situationsRaw);
+
+//Set up the inventory
+barter.inventory = new Inventory();
+barter.inventory.addItems(new ItemStack(barter.items["pencil"], 1));
+barter.situation = "intro";
+
+//Functions
+
+//initialize the page by displaying the first situation
+var init = function () {
+    setupSituation(barter.situation);
+};
+
+//Display a situation on the page
+var setupSituation = function (id) {
+    var situation = barter.situations[id];
+    $("#title").html(situation.title);
+    $("#description").html(situation.description);
+    generateButtons(situation);
+};
+
+//Generate buttons based on a situation's options
+var generateButtons = function (situation) {
+    $("#options").html("");
+    for (var i in situation.options) {
+        var option = situation.options[i];
+        if (!option.onetime || option.onetime && !option.visited) {
+            var button;
+            switch (option.type) {
+                case "goto":
+                    button = goToButton(option);
+                    break;
+                case "give":
+                    button = giveButton(option);
+                    break;
+                case "barter":
+                    button = barterButton(option);
+                    break;
+                case "get":
+                    button = getButton(option);
+                    break;
+                default:
+                    console.log("YOU GET NOTHING! YOU LOSE! GOOD DAY SIR!");
+                    break;
+            }
+            $("#options").append(button);
+        }
+    }
+};
+
+//Create a generic button
+var createButton = function (label, action, option) {
+    var button = document.createElement("button");
+    button.innerHTML = label;
+    button.onclick = action;
+    button.disabled = !meetsRequirements(option);
+    return button;
+};
+
+//Check if the player's inventory has enough items to meet an option's
+//requirements
+var meetsRequirements = function (option) {
+    if (option.requiredItems === null)
+        return true;
+    else
+        return barter.inventory.hasItems(option.requiredItems);
+};
+
+//Create a button that advances to the next situation
+var goToButton = function (option) {
+    var button = createButton(option.name, actionGoTo(option), option);
+    return button;
+};
+
+//Create a button that takes items from the player's inventory
+var giveButton = function (option) {
+    var button = createButton(option.name, actionGive(option), option);
+    return button;
+};
+
+//Create a button that gives items to the player
+var getButton = function (option) {
+    var button = createButton(option.name, actionGet(option), option);
+    return button;
+};
+
+
+//Create a button that will swap player items for new items
+var barterButton = function (option) {
+    var item1 = barter.items[option.requiredItems.id];
+    var item2 = barter.items[option.items.id];
+    var quantity1 = option.requiredItems.quantity;
+    var quantity2 = option.items.quantity;
+    var label = "Barter <b>" + item1.name + " (" + quantity1 + ")</b> for <b>"
+        + item2.name + " (" + quantity2 + ")</b>";
+    var button = createButton(label, actionBarter(option), option);
+    return button;
+};
+
+//Get a lambda function for bartering items
+var actionBarter = function (option) {
+    return function () {
+        visit(option);
+        barter.inventory.removeItems(option.requiredItems);
+        barter.inventory.addItems(option.items);
+        updateInventory();
+        actionGoTo(option)();
+    };
+};
+
+//Get a lambda function for adding items to the inventory
+var actionGet = function (option) {
+    return function () {
+        visit(option);
+        barter.inventory.addItems(option.items);
+        updateInventory();
+        actionGoTo(option)();
+    }
+};
+
+//Get a lambda function for removing items from the inventory
+var actionGive = function (option) {
+    return function () {
+        visit(option);
+        barter.inventory.removeItems(option.requiredItems);
+        updateInventory();
+        actionGoTo(option)();
+    }
+};
+
+//Get a lambda function for going to a new situation
+var actionGoTo = function (option) {
+    return function () {
+        visit(option);
+        barter.situation = option.next;
+        setupSituation(barter.situation);
+    };
+};
+
+//Turn on the visited flag of an option
+var visit = function (option) {
+    option.visited = true;
+};
+
+//Refresh the inventory on the page
+var updateInventory = function () {
+    $("#inventoryContents").html(generateInventory(barter.inventory.stacks));
+};
+
+//Generate the inventory HTML string
+var generateInventory = function (stackArr) {
+    var str = "";
+    for (var stack in stackArr)
+        str += inventoryItemString(stackArr[stack]);
+    return str;
+};
+
+//Generate a table row in the inventory table on the page
+var inventoryItemString = function (stack) {
+    return "<tr><td>"
+            + stack.name
+            + "</td><td>"
+            + stack.description
+            + "</td><td>"
+            + stack.quantity
+            + "</td></tr>";
+};
---
+++ scripts/structures.js
@@ -0,0 +1,113 @@
+﻿//Item data type
+var Item = function (id, name, description, type) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.type = type;
+};
+//Read items from raw data
+var readItems = function (raw) {
+    barter.items = {};
+    for (var key in raw)
+        barter.items[key] = new Item(key, raw[key][0], raw[key][1], raw[key][2]);
+};
+
+//Item Stack data type
+var ItemStack = function (item, quantity) {
+    this.item = item;
+    this.quantity = quantity;
+};
+ItemStack.prototype = {
+    get id() { return this.item.id; },
+    get name() { return this.item.name; },
+    get description() { return this.item.description; },
+    get type() { return this.item.type; }
+};
+
+//Option data type
+var Option = function (id, name, type, next, requiredItem, requiredQuantity, item, quantity, onetime) {
+    this.id = id;
+    this.name = name;
+    this.type = type;
+    this.next = next;
+    this.requiredItems = requiredItem !== null ? new ItemStack(barter.items[requiredItem], requiredQuantity) : null;
+    this.items = item !== null ? new ItemStack(barter.items[item], quantity) : null;
+    this.onetime = onetime;
+    this.visited = false;
+};
+
+//Read options from raw data
+var readOptions = function (raw) {
+    barter.options = {};
+    for (var key in raw)
+        barter.options[key] = new Option(key, raw[key][0], raw[key][1], raw[key][2], raw[key][3], raw[key][4], raw[key][5], raw[key][6], raw[key][7]);
+};
+
+//Situation Manifold data type
+var SituationManifold = function (id, title, description, options) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.options = options;
+};
+
+//Read situations from raw data
+var readSituations = function (raw) {
+    barter.situations = {};
+    for (var key in raw) {
+        var options = [];
+        for (var index in raw[key][2]) {
+            var option = barter.options[raw[key][2][index]];
+            options.push(option);
+        }
+        barter.situations[key] = new SituationManifold(key, raw[key][0], raw[key][1], options);
+    }
+};
+
+//Inventory data structure
+var Inventory = function () {
+    this.stacks = [];
+    this.getStack = function (id) {
+        for (var i in this.stacks) {
+            if (this.stacks[i].id === id)
+                return this.stacks[i];
+        }
+        return null;
+    }
+    //Remove items based on a stack
+    this.removeItems = function (stack) {
+        var index = -1;
+        for (var i in this.stacks) {
+            if (this.stacks[i].id === stack.id) {
+                index = i;
+                break;
+            }
+        }
+        if (index !== -1) {
+            var item = this.stacks[index];
+            if (item.quantity > stack.quantity)
+                item.quantity -= stack.quantity;
+            else
+                this.stacks.splice(index, 1);
+        }
+    }
+    //Add items based on a stack
+    this.addItems = function (stack) {
+        var existingStack = this.getStack(stack.id);
+        if (existingStack === null)
+            this.stacks.push(stack);
+        else
+            existingStack.quantity += stack.quantity;
+    }
+    //Check if this inventory has the desired items, including the proper
+    //quantity
+    this.hasItems = function (stack) {
+        var existingStack = this.getStack(stack.id);
+        if (existingStack === null)
+            return false;
+        else if (existingStack.quantity >= stack.quantity)
+            return true;
+        else
+            return false;
+    }
+};
