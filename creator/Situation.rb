7503fed663f45789d0048effceb1e1168b09b8e7
Peter Gagliardi <ptrgags@live.com>
2016-09-06 15:46:56 -0400

    Add Situation commands
    Added commands for Situation data, minus the file editing.

---
+++ creator/Situation.rb
@@ -0,0 +1,37 @@
+require_relative "ui"
+
+# TODO: Handle Description files
+class Situation
+    attr_accessor :id, :title
+
+    def initialize id
+        @id = id
+        @title = ""
+    end
+
+    # Convert to a hash for JSON
+    def to_hash
+        {
+            "title" => @title
+        }
+    end
+
+    def to_s
+        "#{@id}: #{@title}"
+    end
+
+    # Create a Situation from an ID and hash
+    # This data is from the JSON
+    def self.from_hash id, data
+        situation = self.new id
+        situation.title = data['title']
+        situation
+    end
+
+    # Read user input to populate the situation data
+    def self.from_input id
+        situation = self.new id
+        situation.title = prompt_str "Title for Situation #{id}: "
+        situation
+    end
+end
--- creator/creator.rb
+++ creator/creator.rb
@@ -3,40 +3,105 @@ require 'json'
 
 require_relative 'ui'
 require_relative 'Item'
+require_relative 'Situation'
 
 class StoryCreator
+    
     def initialize
         @items = {}
+        @situations = {}
+    end
+
+    def help
+        puts "StoryCreator Commands:"
+        puts "help - display this help"
+        puts "list_items - list all items"
+        puts "edit_item <id> - edit/create item"
+        puts "delete_item <id> - delete item"
+        puts "list_situations - list all situations"
+        puts "edit_situation <id> - create/edit a situation"
+        puts "delete_situation <id> - delete a situation"
+        puts "save <fname> - save the final JSON file"
+        puts "quit - exit the program, saving data"
+        puts "exit - same as quit"
+    end
+
+    def list_items
+        puts "All Items:"
+        @items.each {|id, item| puts item}
+    end
+
+    def edit_item args
+        _, id = args
+        puts "Edit item with ID #{id}"
+        @items[id] = Item.from_input id
+    end
+
+    def delete_item args
+        _, id = args
+        puts "Delete item with ID #{id}"
+        @items.delete(id)
+    end
+
+    def list_situations
+        puts "All situations:"
+        @situations.each {|id, situation| puts situation}
+    end
+
+    def edit_situation args
+        _, id = args
+        puts "Edit situation with ID #{id}"
+        @situations[id] = Situation.from_input id
+    end
+
+    # TODO: Prompt to delete the text file
+    def delete_situation args
+        _, id = args
+        puts "Delete situation with ID #{id}"
+        @situations.delete(id)
+    end
+
+    def save args
+        _, fname = args
+        puts "saving to #{fname}"
+        # TODO: These should be converted to hashes
+        data = {
+            "items" => @items,
+            "situations" => @situations
+        }
+        File.open(fname, "w") do |f|
+            f.write(JSON.pretty_generate(data))
+        end
+    end
+
+    def bye
+        puts "Bye!"
+        exit 0
     end
 
     def parse_command command
         args = command.split(' ')
         case args[0]
-        when /^list_items/
-            puts "All Items:"
-            @items.each {|id, item| puts item}
-        when /^edit_item/
-            cmd, id = args
-            puts "Edit item with ID #{id}"
-            @items[id] = Item.from_input id
-        when /^delete_item/
-            command, id = args
-            puts "Delete item with ID #{id}"
-            @items.delete(id)
-        when /^save/
-            cmd, fname = args
-            puts "saving to #{fname}"
-            data = {
-                "items" => @items
-            }
-            File.open(fname, "w") do |f|
-                f.write(JSON.pretty_generate(data))
-            end
-        when /^quit/
-            puts "Bye!"
-            exit 0
+        when "list_items"
+            list_items
+        when "edit_item"
+            edit_item args
+        when "delete_item"
+            delete_item args
+        when "list_situations"
+            list_situations
+        when "edit_situation"
+            edit_situation args
+        when "delete_situation"
+            delete_situation args
+        when "save"
+            save args
+        when "help"
+            help
+        when "quit", "exit"
+            bye
         else
-            puts "Bad Command! Try Again"
+            puts "Bad command! Try 'help' for valid commands"
         end
     end
 
@@ -50,17 +115,3 @@ end
 
 creator = StoryCreator.new
 creator.repl
-
-
-#story = JSON.parse(File.read "tutorial.json")
-#items = story['items']
-
-#all_items = items.map {|id, data| Item.from_hash id, data}
-#puts all_items
-
-
-
-#puts Item.from_input "banana"
-
-
-#items.map {|key, val| puts [key, val].inspect}
